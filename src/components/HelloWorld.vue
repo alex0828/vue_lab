58ad499aa3b214f49da93387b0043c0c660422a7
Wuwuyiaewu <okman9527@gmail.com>
2022-06-15 01:49:59 +0800

    init plan B

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,40 @@
-<script lang="ts">
-import { defineComponent, ref } from "vue";
-import Tpi from "../types/Tpi.js";
-export default defineComponent({
-  name: "TypeScript",
-  components: {},
-  setup() {
-    const tpis = ref<Tpi[]>([
-      { title: "資料中心", name: "pinia", id: "1" },
-      { title: "開發模式", name: "compostion API", id: "2" },
-      { title: "規範語言", name: "TypeScript", id: "3" },
-      { title: "建構打包工具", name: "Vite", id: "4" },
-    ]);
-    return { tpis };
-  },
-});
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
 </script>
 
 <template>
-  <p v-for="tpi of tpis" :key="tpi.id">使用的{{ tpi.title }}為{{ tpi.name }}</p>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
 }
 </style>
